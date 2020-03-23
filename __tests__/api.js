22eeb285be5fafefcb77819716c86f57a0f31084
https://github.com/AlMuthana1992/m.hasan.git <test@test.test>
2020-03-23 12:35:00 +0100

    ändert

--- __tests__/api.js
+++ __tests__/api.js
@@ -1,30 +1,41 @@
-const express = require("express");
-const app = require('../app')
-const request = require('supertest')
-let server;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var express = require('express');
+var app = express();
 
-const app = express();
-app.get('/',function(req,res){
-  res.send('server ist läuft')
-})
-describe('API Request', () => {
-  test('should be 200', async done => {
-    const response = await request(app)
-    .get('/api')
-    expect(response.body).toEqual([{user: 'fake', password: 'fake'}])
-    done();
-  })
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-beforeAll(async (done) => {
-  server = app.listen(3000, () => {
-    global.agent = request.agent(server);
-    done();
-  });
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-afterAll(async () => {
-  await server.close();
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
 module.exports = app;
